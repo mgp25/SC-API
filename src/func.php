745878b3d8c01fae4b90cfd8c058613c38f79951
mgp25 <me@mgp25.com>
2015-05-11 10:09:52 +0200

    update func

--- src/func.php
+++ src/func.php
@@ -1,28 +1,36 @@
 <?php
-
-function text($image, $text, $leftY = 500, $png=false){
-	$leftY = $leftY + (0.054 * imagesy($image))/2;
+function text($image, $text)
+{
+    //create resource from image page
+    $image = imagecreatefromjpeg($image);
     $font = __DIR__ . '/fonts/Roboto-Light.ttf';
+    //leftmost side of image
     $leftX = 0;
+    //width of image
     $rightX = imagesx($image);
-    $rightY = $leftY - (0.054 * imagesy($image));
-    $black = imagecolorallocatealpha($image, 0, 0, 0, 50);
+    //half the height of the image
+    $leftY = imagesy($image) / 2;
+    //high with padding added for text height, scaled
+    $rightY = $leftY - (0.04 * imagesy($image));
+    //create rgb(0, 0, 0) with 0.75 alpha for opacity for box background
+    $black = imagecolorallocatealpha($image, 0, 0, 0, 75);
+    //create rgb(255, 255, 255) for text color
     $whiteText = imagecolorallocate($image, 255, 255, 255);
+    //add rectangle
     imagefilledrectangle($image, $leftX, $leftY, $rightX, $rightY, $black);
-    $boundingBox = imagettfbbox((0.021 * imagesy($image)), 0, $font, $text);
-    $box = imagettfbbox((0.021 * imagesy($image)), 0, $font, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"); //Tallest character
-    $textheight = abs($box[3] - $box[5]);
+    //get bounding box for text
+    $boundingBox = imagettfbbox((0.03 * imagesy($image)), 0, $font, $text);
+    //calculate leftmost x position for text placement to be in center
     $x = ($rightX - ($boundingBox[0] + $boundingBox[2])) / 2;
-    imagettftext($image, (0.021 * imagesy($image)), 0, $x, $leftY - (((0.054 * imagesy($image)) - $textheight) /2), $whiteText, $font, $text);
-    ob_start();
-    if ($png){
-        imagepng($image);
-    }else{
-        imagejpeg($image);
-    }
-    $image_data = ob_get_contents();
-    ob_end_clean();
+    //add text to image in box
+    imagettftext($image, (0.03 * imagesy($image)), 0, $x, ($leftY - ($leftY * 0.015)), $whiteText, $font, $text);
+    //save new image with text as jpeg
+    imagejpeg($image, __DIR__ . "/cache/image.jpg");
+    //get image data
+    $fileData = file_get_contents(__DIR__ . "/cache/image.jpg");
+    //remove image from memory and remove from disk
     imagedestroy($image);
-    return $image_data;
+    unlink(__DIR__ . '/cache/image.jpg');
+    //return jpeg data
+    return $fileData;
 }
-?>
