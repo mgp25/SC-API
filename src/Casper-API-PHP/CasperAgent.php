41424592eb22b605e2fc420207ed58b3af30e5a7
mgp25 <me@mgp25.com>
2015-10-18 22:41:02 +0200

    Fixes #238

--- src/Casper-API-PHP/CasperAgent.php
+++ src/Casper-API-PHP/CasperAgent.php
@@ -145,6 +145,8 @@ abstract class CasperAgent {
 			}
 			curl_setopt($ch, CURLOPT_POST, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 20);
 		}
 
 		$result = curl_exec($ch);
@@ -153,8 +155,7 @@ abstract class CasperAgent {
 		//we provide ours (see http://curl.haxx.se/docs/sslcerts.html).
 		if(curl_errno($ch) == 60){
 			curl_setopt($ch, CURLOPT_CAINFO, __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."ca_bundle.crt");
-			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,0);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+
 			$result = curl_exec($ch);
 		}
 
